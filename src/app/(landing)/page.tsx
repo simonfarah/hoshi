bd29945b20370f18d3c0c4cba986a9657485733d
Simon Farah <88495690+simonfarah@users.noreply.github.com>
2024-03-20 14:08:55 +0200

    fix: homepage auth verification

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,6 +1,8 @@
 import { type Metadata } from 'next';
+import { redirect } from 'next/navigation';
 import { Link } from '#/components/ui/link';
 import { validateRequest } from '#/lib/auth/validate-request';
+import { REDIRECTS } from '#/lib/constants';
 import { UserDetails } from './_components/user-details';
 
 export const metadata: Metadata = {
@@ -9,6 +11,10 @@ export const metadata: Metadata = {
 
 export default async function HomePage() {
   const { user } = await validateRequest();
+
+  if (user && !user.emailVerified) {
+    redirect(REDIRECTS.toVerify);
+  }
 
   return (
     <section className="flex min-h-svh items-center justify-center p-5">
