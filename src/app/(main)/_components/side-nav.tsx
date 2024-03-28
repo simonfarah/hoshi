647f196cdbdcf3baddb31453e868a17686a29080
Simon Farah <88495690+simonfarah@users.noreply.github.com>
2024-03-28 13:19:30 +0200

    add: sign out button

--- src/app/(main)/_components/side-nav.tsx
+++ src/app/(main)/_components/side-nav.tsx
@@ -1,5 +1,10 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
 import { Icons } from '#/components/icons';
 import { Logo } from '#/components/logo';
+import { Button } from '#/components/ui/button';
 import { Link } from '#/components/ui/link';
 import {
   Tooltip,
@@ -7,8 +12,23 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from '#/components/ui/tooltip';
+import { REDIRECTS } from '#/lib/constants';
+import { api } from '#/trpc/react';
 
 export function SideNav() {
+  const router = useRouter();
+
+  const { mutate: signOut, isLoading } = api.auth.signOut.useMutation({
+    onSuccess: (res) => {
+      toast.success(res.success);
+      router.push(REDIRECTS.afterSignOut);
+      router.refresh();
+    },
+    onError: (e) => {
+      toast.error(e.message);
+    },
+  });
+
   return (
     <aside className="inset-y fixed left-0 z-20 flex h-full flex-col border-r border-border">
       <div className="border-b border-border p-2">
@@ -56,9 +76,15 @@ export function SideNav() {
 
           <Tooltip>
             <TooltipTrigger asChild>
-              <Link href="/dashboard" variant="ghost" size="icon">
+              <Button
+                variant="ghost"
+                size="icon"
+                className="hover:bg-red-500 hover:text-white"
+                onClick={() => signOut()}
+                disabled={isLoading}
+              >
                 <Icons.signOut className="h-5 w-5" />
-              </Link>
+              </Button>
             </TooltipTrigger>
             <TooltipContent side="right" sideOffset={5}>
               Sign out
